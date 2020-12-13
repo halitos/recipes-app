5116a6b2f224ab297df3e9324e936562a63463dc
halit <oskan96@gmail.com>
2020-12-13 12:40:27 +0000

    eslint config

--- src/Components/HealthLabels.js
+++ src/Components/HealthLabels.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const HealthLabels = ({recipes}) => {
-    return (
-            <ul>
-            {recipes[0].recipe.healthLabels.map((label, i) => {
-                   return <li key={i}> {label} </li>  })
-                   }
-            </ul>          
-    )
-}
+const HealthLabels = ({ recipes }) => {
+  return (
+    <ul>
+      {recipes[0].recipe.healthLabels.map((label, i) => {
+        return <li key={i}> {label} </li>;
+      })}
+    </ul>
+  );
+};
 
 export default HealthLabels;
